1e611117da10ea0ceace92daf6bc0e3b32a9caf8
emf09 <93343039+fuhgeiu@users.noreply.github.com>
2025-11-16 23:31:27 -0600

    1.1

--- app/src/test/java/org/example/RemoveDuplicatesTest.java
+++ app/src/test/java/org/example/RemoveDuplicatesTest.java
@@ -1,7 +1,7 @@
 package org.example;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
